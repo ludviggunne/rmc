29f3de79c4c90a5987c3d72f1097f2dc50cd9f57
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2025-01-22 09:12:56 +0100

    create runtime dir if it doesn't exist

--- get_runtime_dir.c
+++ get_runtime_dir.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/limits.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #include "get_runtime_dir.h"
 
@@ -16,6 +18,10 @@ const char *get_runtime_dir(void)
       exit(EXIT_FAILURE);
     }
     snprintf(s_runtime_dir, sizeof(s_runtime_dir), "%s/rmc", xdg_runtime_dir);
+    if (mkdir(s_runtime_dir, 0777) < 0 && errno != EEXIST) {
+      perror("mkdir");
+      exit(EXIT_FAILURE);
+    }
   }
   return s_runtime_dir;
 }
