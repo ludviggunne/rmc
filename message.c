1a1ef3b3c1d3bb029a13c7dc0f8915d3af43bd7b
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2025-01-22 15:31:40 +0100

    rewrite message protocol

--- message.c
+++ message.c
@@ -1,126 +1,95 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include "message.h"
 
-/* Protocol for command messages:
- *        1. length of working directory string,
- *           including null character.
- *        2. length of command string, including
- *           null character.
- *        3. number of environment variables `nenv`.
- *        4. the working directory string.
- *        5. the command string.
- *        6. `nenv` number of entries of the form
- *           6.1. length of key-value pair, including
- *                null character.
- *           6.2  key-value pair (i.e. 'VAR=DEF').
- */
-
-struct spec {
-  size_t cwdlen;
-  size_t cmdlen;
-  size_t nenv;
-};
-
-static int read_all(int fd, char *ptr, size_t len)
+int msg_getline(char **lineptr, FILE *f)
 {
-  ssize_t r;
-  size_t off = 0;
-  while (off < len) {
-    r = read(fd, ptr + off, len - off);
-    if (r <= 0)
-      return -1;
-    off += r;
+  size_t len;
+  /* We won't be reusing any memory */
+  *lineptr = NULL;
+  if (getline(lineptr, &len, f) < 0) {
+    return -1;
   }
-  return 0;
-}
-
-static int write_all(int fd, const char *ptr, size_t len)
-{
-  ssize_t w;
-  size_t off = 0;
-  while (off < len) {
-    w = write(fd, ptr + off, len - off);
-    if (w <= 0)
-      return -1;
-    off += w;
-  }
+  /* Get rid of newline */
+  (*lineptr)[strlen(*lineptr) - 1] = 0;
   return 0;
 }
 
 int read_message(int fd, struct message *msg)
 {
-
-  struct spec spec;
-
-  if (read_all(fd, (char *)&spec, sizeof(spec)) < 0)
+  FILE *f = fdopen(fd, "r");
+  if (f == NULL)
     return -1;
 
-  msg->cwd = malloc(spec.cwdlen);
-  msg->cmd = malloc(spec.cmdlen);
-  msg->env = calloc(spec.nenv + 1, sizeof(*msg->env));
+  memset(msg, 0, sizeof(*msg));
 
-  if (!(msg->cwd && msg->cmd && msg->env)) {
-    free_message(msg);
-    return -1;
-  }
-  if (read_all(fd, msg->cwd, spec.cwdlen) < 0) {
-    free_message(msg);
-    return -1;
-  }
-  if (read_all(fd, msg->cmd, spec.cmdlen) < 0) {
+  if (msg_getline(&msg->cwd, f) < 0) {
     free_message(msg);
     return -1;
   }
 
-  for (size_t i = 0; i < spec.nenv; ++i) {
-    size_t len;
-    if (read_all(fd, (char *)&len, sizeof(len)) < 0) {
+  if (msg_getline(&msg->cmd, f) < 0) {
+    free_message(msg);
+    return -1;
+  }
+
+  char *nenv_buf = NULL;
+  size_t nenv;
+
+  if (msg_getline(&nenv_buf, f) < 0) {
+    free(nenv_buf);
+    free_message(msg);
+    return -1;
+  }
+
+  if (sscanf(nenv_buf, "%lu", &nenv) != 1) {
+    free(nenv_buf);
+    free_message(msg);
+    return -1;
+  }
+  free(nenv_buf);
+
+  msg->env = calloc(nenv, sizeof(*msg->env) + 1);
+  for (size_t i = 0; i < nenv; ++i) {
+    if (msg_getline(&msg->env[i], f) < 0) {
       free_message(msg);
       return -1;
     }
-    msg->env[i] = malloc(len);
-    if (read_all(fd, msg->env[i], len) < 0) {
-      free_message(msg);
-      return -1;
-    }
   }
 
-  msg->env[spec.nenv] = NULL;
+  msg->env[nenv] = NULL;
 
   return 0;
 }
 
 int write_message(int fd, struct message *msg)
 {
-
-  struct spec spec;
-
-  spec.cwdlen = strlen(msg->cwd) + 1;
-  spec.cmdlen = strlen(msg->cmd) + 1;
-
-  spec.nenv = 0;
-  char **envptr = msg->env;
-  for (; *envptr; ++envptr)
-    ++spec.nenv;
-
-  if (write_all(fd, (const char *)&spec, sizeof(spec)) < 0)
-    return -1;
-  if (write_all(fd, msg->cwd, spec.cwdlen) < 0)
-    return -1;
-  if (write_all(fd, msg->cmd, spec.cmdlen) < 0)
+  FILE *f = fdopen(fd, "w");
+  if (f == NULL)
     return -1;
 
-  envptr = msg->env;
-  for (; *envptr; ++envptr) {
-    size_t len = strlen(*envptr) + 1;
-    if (write_all(fd, (char *)&len, sizeof(len)) < 0)
-      return -1;
-    if (write_all(fd, *envptr, len) < 0)
+  size_t nenv = 0;
+  for (char **envptr = msg->env; *envptr; ++envptr)
+    nenv++;
+
+  if (fprintf(f, "%s\n", msg->cwd) < 0)
+    return -1;
+
+  if (fprintf(f, "%s\n", msg->cmd) < 0)
+    return -1;
+
+  if (fprintf(f, "%lu\n", nenv) < 0)
+    return -1;
+
+  for (char **envptr = msg->env; *envptr; ++envptr) {
+    if (fprintf(f, "%s\n", *envptr) < 0)
       return -1;
   }
+
+  fflush(f);
 
   return 0;
 }
