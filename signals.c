7876b1fbe3698554f239e173ac8ef4794767d050
Ludvig Gunne Lindström <ludviggunnelindstrom@gmail.com>
2025-01-22 09:04:41 +0100

    static storage for signalfd

--- signals.c
+++ signals.c
@@ -3,24 +3,29 @@
 #include <stdlib.h>
 #include <sys/signalfd.h>
 
+static int s_fd = -1;
+
 int get_signalfd(void)
 {
   sigset_t mask;
-  (void)sigemptyset(&mask);
-  (void)sigaddset(&mask, SIGINT);
-  (void)sigaddset(&mask, SIGTERM);
-  (void)sigaddset(&mask, SIGCHLD);
-  (void)sigaddset(&mask, SIGUSR1);
-  if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0) {
-    perror("sigprocmask");
-    exit(EXIT_FAILURE);
+
+  if (s_fd < 0) {
+    (void)sigemptyset(&mask);
+    (void)sigaddset(&mask, SIGINT);
+    (void)sigaddset(&mask, SIGTERM);
+    (void)sigaddset(&mask, SIGCHLD);
+    (void)sigaddset(&mask, SIGUSR1);
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0) {
+      perror("sigprocmask");
+      exit(EXIT_FAILURE);
+    }
+    s_fd = signalfd(-1, &mask, SFD_CLOEXEC);
+    if (s_fd < 0) {
+      perror("signalfd");
+      exit(EXIT_FAILURE);
+    }
   }
-  int fd = signalfd(-1, &mask, SFD_CLOEXEC);
-  if (fd < 0) {
-    perror("signalfd");
-    exit(EXIT_FAILURE);
-  }
-  return fd;
+  return s_fd;
 }
 
 void unblock_signals(void)
